844a77dd5e07d8266b50fb0284316abc4930fe2a
Sajid Mahamud <83598348+sajidmahamud835@users.noreply.github.com>
2024-09-16 06:50:04 +0000

    updating configs

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+export const dynamic = 'force-dynamic'
+
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-ibm-plex-serif'
+})
+
+export const metadata: Metadata = {
+  title: "BankSync",
+  description: "BankSync is a next-level banking application that unifies all your traditional bank accounts and Web3 wallets in one platform, built with Next.js for modern, seamless financial management.",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
+    </html>
+  );
+}
