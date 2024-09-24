3093d47633db41fcb8bb10d0399bde058ddb19d8
sajidmahamud835 <sajidmahamud835@gmail.com>
2024-09-24 19:05:59 +0600

    cleaning code

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -8,15 +8,7 @@ import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
+import { Form } from "@/components/ui/form"
 import CustomInput from './CustomInput';
 import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
